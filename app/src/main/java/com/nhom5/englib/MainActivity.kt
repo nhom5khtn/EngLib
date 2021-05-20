4b32e2d5c55d1e3b1e2168a08983e674fac8fcf4
= <=>
2021-05-20 23:49:45 +0700

    Update SplashActivity and Dialog

--- app/src/main/java/com/nhom5/englib/MainActivity.kt
+++ app/src/main/java/com/nhom5/englib/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.nhom5.englib
 
+import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 
 class MainActivity : AppCompatActivity() {
+    override fun onBackPressed() {
+        var builder = AlertDialog.Builder(this)
+        builder.setTitle("Wrong reply")
+        builder.setMessage("Do you want to quit?")
+        builder.setPositiveButton("Give Up!", { dialogInterface: DialogInterface, i: Int ->
+            finish()
+        })
+        builder.setNegativeButton("Continue!", { dialogInterface: DialogInterface, i: Int ->
+        })
+        builder.show()
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Toast.makeText(this, "Welcome", Toast.LENGTH_SHORT).show()
+
     }
 }
